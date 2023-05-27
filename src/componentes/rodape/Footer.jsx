2187a1154f26925bda2b37c5edef8a8cfb9c5611
Douglas <121909515+Douglaslima93@users.noreply.github.com>
2023-05-27 06:54:17 -0300

    Atualizar o Footer.jsx

--- src/componentes/rodape/Footer.jsx
+++ src/componentes/rodape/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer () {
     return (
         <footer>
-
+        <span>Todos os direitos reservados a óticas olhar misterioso.<span/>
         </footer>
     )
 }
