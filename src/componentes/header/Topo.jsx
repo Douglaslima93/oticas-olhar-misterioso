33505a24813ca82bc03aa1e03d63a3e2db199a76
Douglas Lima <douglas07a@gmail.com>
2023-05-29 14:20:05 -0300

    section topo estilo concluido

--- src/componentes/header/Topo.jsx
+++ src/componentes/header/Topo.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import './topo.css'
 
 function Topo () {
     return (
-        <header id="#topo">
+        <header id="#topo" className="menu">
+            <div>
+                <img/>
+            </div>
+            <div className="icones-menu">
             <a href="#produtos">PRODUTOS</a>
             <a href="#sobre">SOBRE</a>
             <a href="#contato">CONTATO</a>
+            </div>
         </header>
     )
 }
--- src/componentes/secao/SecaoCapa.jsx
+++ src/componentes/secao/SecaoCapa.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import './secaocapa.css'
 
 function SecaoCapa () {
     return (
-        <section>
+        <section className="secaocapa">
+            <div className="itens-secaocapa">
            <p>Preços baixos em</p>
-           <h1>ÓCULOS DE GRAU E DE SOL</h1>
+           <h1>ÓCULOS DE<br/> GRAU E DE SOL</h1>
            <p>Você só encontra aqui</p>
+           </div>
         </section>  
     )
 }
