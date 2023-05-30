933f73d371601b1a5f71f7dc7f96137bd2441197
Douglas Lima <douglas07a@gmail.com>
2023-05-30 18:11:33 -0300

    section sobre concluida

--- src/componentes/secao/SecaoSobre.jsx
+++ src/componentes/secao/SecaoSobre.jsx
@@ -1,26 +1,39 @@
 import React from "react";
+import './secaosobre.css'
 
 function SecaoSobre () {
     return (
-        <section id="sobre">
-            <div>
+        <section id="sobre" className="sobre">
+            <div className="container-sobre">
+                <div className="container-texto-sobre">
                 <h2>QUEM SOMOS NÓS?</h2>
                 <p>Fundada em 2001, em Boituva - São Paulo, a Ótica Olhar Misterioso iniciou suas atividades
                     focada no atendimento ao público de renda baixa, sempre com o objetivo de proporcionar
                     ao cliente bom atendimento, qualidade e preço baixo.
                 </p>
-                <div>
-                    <img src="./img/loja.png" alt="loja"/>
-                    <div>
+                </div>
+                <div className="container-cards">
+
+                    <div className="loja">
+                    
+                    </div>
+
+                    <div className="texto-cards">
                         <h2>NOSSAS FILIAIS</h2>
                         <p>Hoje temos mais de 20 filiais pelo Brasil e na América.</p>
                     </div>
 
-                    <img src="./img/atendimento.png" alt="atendimento"/>
-                    <div>
+                    <div className="texto-cards">
                         <h2>ATENDIMENTO FLEXIVEL</h2>
                         <p>Nossa equipe possui é treinada para te atender</p>
-                        </div>
+                    </div>
+
+                    <div className="atendimento">
+                    
+                    </div>
+
+                    
+
                 </div>
             </div>
         </section>
