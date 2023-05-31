a511f145b6a93df9b93d5abf1a3af9e0a52d19a2
Douglas Lima <douglas07a@gmail.com>
2023-05-31 11:11:10 -0300

    section contato concluida

--- src/componentes/secao/SecaoContato.jsx
+++ src/componentes/secao/SecaoContato.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+import './secaocontato.css'
 
 function SecaoContato () {
     return (
         <section id="contato">
-            <div>
+            <div className="container-contato">
+                <div className="faleconosco">
                 <h2>FALE CONOSCO</h2>
                 <p>Não perca tempo, venha conhecer uma de nossas lojas ou entre em contato atráves de nossas redes sociais
                     ou central de atendimento.
                 </p>
+                </div>
 
-                <div>
-                    <div>
+                <div className="container-contato-rede">
+                    <div className="contato">
                         <h3>Contato</h3>
                         <a href=""><img src="./img/local.png" alt="local"/>Boituva , SP</a>
                         <a href="tel:+551599999999"><img src="./img/telefone.png" alt="telefone"/>(15) 9999-9999</a>
                         <a href="mailto:contato@oticaolharmisterioso.com"><img src="./img/email.png" alt="email"/>contato@oticaolharmisterioso.com</a>
                     </div>
 
-                    <div>
+                    <div className="redes-sociais">
                         <h3>Nossas Redes Sociais</h3>
                         <a href="https://www.facebook.com/login"><img src="./img/fb.png" alt="Facebook"/>/OticaOlharMisterioso</a>
                         <a href="https://www.instagram.com/"><img src="./img/ig.png" alt="Instagram"/>@oticaolharmisteriososp</a>
